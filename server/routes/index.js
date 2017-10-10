654f9b16b44c568e7acb057fbde8b5bc3f0e10f9
JiangBao <873575504@qq.com>
2017-10-10 14:13:23 +0800

    update server

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,16 @@ let weatherController = require('../controllers/weather.js');
 
 let Route = (app) => {
   /**
+   * allow cross origin request
+   */
+  app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+  })
+
+  /**
    * get weather data by city
    */
   app.get('/weather/:city', weatherController.handleGetWeatherByCity);
